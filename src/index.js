f0f2dd450d4393db04879adaf6c61c172411a393
78fatemanazari <fatemanazari78@gmail.com>
2023-09-06 19:36:31 +0500

    Linters fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; 
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-   </Provider>,
+    </Provider>
+    ,
   </React.StrictMode>,
-); 
-
+);
