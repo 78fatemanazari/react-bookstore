f76f53879613fc298c2c18a7993497af8de86e85
fatemanazari <fatemanazari78@gmail.com>
2023-09-08 16:51:39 +0330

    Fix errors

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,16 +6,16 @@ const Navigation = () => {
   const navLinkStyles = ({ isActive }) => ({
     opacity: isActive ? 1 : 0.5,
   });
-  return(
-  <div className="navigation">
-    <div className="nav-items">
-      <h2 className="title">Bookstore CMS</h2>
-      <NavLink className="pages" style={navLinkStyles} to="/">BOOKS</NavLink>
-      <NavLink className="pages" style={navLinkStyles} to="/categories">CATEGORIES</NavLink>
+  return (
+    <div className="navigation">
+      <div className="nav-items">
+        <h2 className="title">Bookstore CMS</h2>
+        <NavLink className="pages" style={navLinkStyles} to="/">BOOKS</NavLink>
+        <NavLink className="pages" style={navLinkStyles} to="/categories">CATEGORIES</NavLink>
+      </div>
+      <img src={Profile} alt="Account user" width="22" height="22" />
     </div>
-    <img src={Profile} alt="Account user" width="22" height="22" />
-  </div>
-);
-  };
+  );
+};
 
 export default Navigation;
